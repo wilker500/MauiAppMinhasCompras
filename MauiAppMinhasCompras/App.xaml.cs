71f382dee7faca05db3d363704bcc9eb74c619e7
Wilker <wilkerbranquinho@gmail.com>
2025-02-17 21:27:30 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+    }
+}
