ee9df96b14826a0858bbc1f33171a986e6cf177a
Wilker <wilkerbranquinho@gmail.com>
2025-03-04 07:13:31 -0300

    Agenda 3 Lista Produto e Novo Produto

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,9 +1,38 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db 
+        {
+
+            get
+            {
+                // Isso verifica se tem objeto dentro de campo DB se não tiver instancia DB;
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                       "banco_sqlite_compras.db3"); 
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
+        
+
+       
+
+
         public App()
         {
+            
+
             InitializeComponent();
 
             // MainPage = new AppShell();
